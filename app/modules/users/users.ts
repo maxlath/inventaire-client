d7a67cff5af044014d5d8dc4ca6d61c765bc326d
jums <jums@inventaire.io>
2024-12-03 14:51:18 +0100

    make user followers as modal

--- app/modules/users/users.ts
+++ app/modules/users/users.ts
@@ -147,13 +147,20 @@ async function showUserContributions (idOrUsernameOrModel, filter) {
   }
 }
 
-async function showUserFollowers (username) {
+async function showUserFollowers (idOrUsernameOrModel) {
   try {
-    const { default: ActorFollowers } = await import('#activitypub/components/actor_followers.svelte')
-    app.layout.showChildComponent('main', ActorFollowers, {
+    const [
+      { default: UsersHomeLayout },
+      user,
+    ] = await Promise.all([
+      import('#users/components/users_home_layout.svelte'),
+      app.request('resolve:to:user', idOrUsernameOrModel),
+    ])
+    app.layout.showChildComponent('main', UsersHomeLayout, {
       props: {
-        actorName: username,
-        standalone: true,
+        showUserFollowers: true,
+        user,
+        actorName: user.username,
       },
     })
   } catch (err) {
